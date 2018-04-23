952d3270f0f03b9d1851d595c6e83a1f415938c7
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-04-22 19:01:09 -0600

    a

--- src/components/movie.js
+++ src/components/movie.js
@@ -4,6 +4,8 @@ import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { Image } from 'react-bootstrap'
 import { withRouter } from "react-router-dom";
 import {fetchMovie} from "../actions/movieActions";
+import {setReview} from "../actions/movieActions";
+import { Col, Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 //support routing by creating a new component
 
@@ -15,7 +17,37 @@ class Movie extends Component {
             dispatch(fetchMovie(this.props.movieId));
     }
 
+    constructor(props) {
+        super(props);
+        this.updateDetails = this.updateDetails.bind(this);
+        this.submitReview = this.submitReview.bind(this);
+        this.review = this.review.bind(this);
+
+        this.state = {
+            details:{
+                movietitle: this.props.selectedMovie.title,
+                review: '',
+                rating: ''
+            }
+        };
+    }
+
+    updateDetails(event){
+        let updateDetails = Object.assign({}, this.state.details);
+
+        updateDetails[event.target.id] = event.target.value;
+        this.setState({
+            details: updateDetails
+        });
+    }
+
+    submitReview() {
+        const {dispatch} = this.props;
+        dispatch(setReview(this.state.details));
+    }
+
     render() {
+
         const ActorInfo = ({actors}) => {
             return actors.map((actor, i) =>
                 <p key={i}>
@@ -31,7 +63,37 @@ class Movie extends Component {
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
             );
-        }
+        };
+
+        const AddReview = () => {
+            return (
+                <Form horizontal>
+                    <FormGroup controlId="review">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Review
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails} value={this.state.details.review} type="review" placeholder="Review" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup controlId="rating">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Rating
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails} value={this.state.details.rating} type="rating" placeholder="Rating" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button onClick={this.submitReview}>Submit</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+            )
+        };
 
         const DetailInfo = ({currentMovie}) => {
             if (!currentMovie) { // evaluates to true if currentMovie is null
@@ -43,13 +105,17 @@ class Movie extends Component {
                     <Panel.Body><Image className="image" src={currentMovie.imageUrl} thumbnail /></Panel.Body>
                     <ListGroup>
                         <ListGroupItem>{currentMovie.title}</ListGroupItem>
+                        <ListGroupItem>{currentMovie.genre}</ListGroupItem>
+                        <ListGroupItem>{currentMovie.year}</ListGroupItem>
                         <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
                         <ListGroupItem><h4><Glyphicon glyph={'star'} /> {currentMovie.avgRating} </h4></ListGroupItem>
                     </ListGroup>
                     <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+                    <Panel.Body><AddReview /></Panel.Body>
                 </Panel>
             );
         };
+
         return (
             <DetailInfo currentMovie={this.props.selectedMovie} />
         );
@@ -61,7 +127,7 @@ const mapStateToProps = (state, ownProps) => {
     return {
         selectedMovie: state.movie.selectedMovie,
         movieId: ownProps.match.params.movieId
-    }
+    };
 }
 
 export default withRouter(connect(mapStateToProps)(Movie));
